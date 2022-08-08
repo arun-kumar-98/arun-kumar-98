5f4945780d4799db8cf553a42e5a6492e78903d3
tectoro <arunrits15@gmail.com>
2022-08-08 12:50:26 +0530

    payment integration with payTM

---
+++ paytm-payment/src/test/java/com/main/PaytmPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytmPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
