0417f4883cf447f16de51477bf3d42193005086c
tectoro <arunrits15@gmail.com>
2022-08-08 12:18:31 +0530

    OAuth with facebook

---
+++ facebook-login-oauth2/src/main/java/com/main/FacebookLoginOauth2Application.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookLoginOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookLoginOauth2Application.class, args);
+	}
+
+}
